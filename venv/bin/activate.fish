f9d7e9fb83136f59d9005004f4f339d7ca103954
antohneotm <antohneotm@gmail.com>
2024-10-03 23:01:46 -0700

    Tic Tac Toe build

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ant.oh.neo/Desktop/MinimaxTicTacToe/venv"
+set -gx VIRTUAL_ENV "/Users/ant.oh.neo/Github/minimax-tictactoe/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
